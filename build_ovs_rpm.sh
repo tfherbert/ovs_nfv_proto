5fd7e9fd108a19e1580afbf3bfbbe80d32bdc767
Thomas F Herbert <thomasfherbert@gmail.com>
2016-01-18 13:49:06 +0000

    Prototype script to build and test Pritesh nsh RPMs

---
+++ build_ovs_rpm.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+set -e
+declare -i CNT
+
+HOME=`pwd`
+TOPDIR=$HOME
+TMPDIR=$TOPDIR/ovsrpm
+
+if [ -d $TMPDIR ]
+then
+    rm -rf $TMPDIR
+fi
+
+sudo yum install -y autoconf automake libtool rpm-build redhat-rpm-config
+
+TAG=nsh-v8
+VERSION=2.3.90
+os_type=rhel6
+kernel_version=3.13.1
+
+mkdir -p $TMPDIR
+
+cd $TMPDIR
+
+mkdir -p $HOME/rpmbuild/RPMS
+mkdir -p $HOME/rpmbuild/SOURCES
+mkdir -p $HOME/rpmbuild/SPECS
+mkdir -p $HOME/rpmbuild/SRPMS
+
+RPMDIR=$HOME/rpmbuild
+
+
+echo "---------------------"
+echo "Clone git repo $TAG"
+echo
+git clone https://github.com/pritesh/ovs
+
+cd ovs
+echo "--------------------"
+echo "Checkout OVS $TAG"
+echo
+if [[ ! "$TAG" =~ "master" ]]; then
+    git checkout $TAG
+fi
+./boot.sh
+#./configure
+./configure --with-linux=/lib/modules/`uname -r`/build
+echo "--------------------"
+echo "Make OVS $TAG"
+echo
+make
+make dist
+
+echo cp openvswitch-$VERSION.tar.gz $HOME/rpmbuild/SOURCES
+cp openvswitch-$VERSION.tar.gz $HOME/rpmbuild/SOURCES
+
+echo "Building kernel module..."
+rpmbuild -bb -D "kversion $kernel_version" -D "kflavors default" --define "_topdir `echo $RPMDIR`" --without check rhel/openvswitch-kmod-${os_type}.spec
+echo " Kernel RPM built!"
+
+echo "Building User Space..."
+rpmbuild -bb "x86_64" --define "_topdir `echo $RPMDIR`" --without check rhel/openvswitch.spec
+
+
